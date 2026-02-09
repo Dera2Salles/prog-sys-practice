a1f0538b5624fbc1dbe9a3aafc2eb311d212fa32
Dera2S <dera.ah.14@gmail.com>
2026-02-09 17:18:41 +0300

    handle error and use main argument

--- search_low_level.c
+++ search_low_level.c
@@ -6,10 +6,24 @@
 #define BUF_SIZE 20
 #define WORD_SIZE 64
 
+int isEndOfSentence(char c) {
+  return (c == ' ' || c == '\n' || c == '\t' || c == '.' || c == ',');
+}
+
 int main(int argc, char *argv[]) {
 
-  int fd = open("out/toto.txt", O_RDWR);
+  if (argc < 4) {
+    fprintf(
+        stderr,
+        "Usage: %s <fichier> <mot_a_chercher> <mot_de_remplacement>\n"
+        "Example:\n"
+        "  %s out/toto.txt toto niov\n\n"
+        "  Attention: remplacement par écrasement (même taille recommandée)\n",
+        argv[0], argv[0]);
+    return 1;
+  }
 
+  int fd = open(argv[1], O_RDWR);
   if (fd == -1) {
     perror("open");
     return 1;
@@ -17,23 +31,23 @@ int main(int argc, char *argv[]) {
 
   char buffer[BUF_SIZE];
   char word[WORD_SIZE];
-  char search[] = "toto";
   int wpos = 0;
   ssize_t byte_read;
 
   while ((byte_read = read(fd, buffer, BUF_SIZE)) > 0) {
-
     for (int i = 0; i < byte_read; i++) {
       char caracter = buffer[i];
-      if (caracter == ' ' || caracter == '\n' || caracter == '\t' ||
-          caracter == '.' || caracter == ',') {
+
+      if (isEndOfSentence(caracter)) {
         if (wpos > 0) {
           word[wpos] = '\0';
-          if (strcmp(search, word) == 0) {
 
+          if (strcmp(argv[2], word) == 0) {
             lseek(fd, -(byte_read - i + wpos), SEEK_CUR);
-            write(fd, "niova", strlen("niova"));
+
+            write(fd, argv[3], strlen(argv[3]));
           }
+
           wpos = 0;
         }
       } else {
