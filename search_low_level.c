847c1bf982551812b8e3e373a6a7ba7d549f4c7d
Dera2S <dera.ah.14@gmail.com>
2026-02-10 14:49:15 +0300

    Update for features like grep

--- search_low_level.c
+++ search_low_level.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -14,14 +15,34 @@ int main(int argc, char *argv[]) {
 
   if (argc < 3) {
     fprintf(stderr,
-            "Usage: %s <fichier> <mot_a_chercher>\n"
+            "Usage: %s [-i] <fichier> <mot_a_chercher>\n"
+            "Options:\n"
+            "  -i  recherche par sous-chaîne\n"
             "Example:\n"
-            "  %s out/toto.txt toto\n",
-            argv[0], argv[0]);
+            "  %s out/toto.txt toto\n"
+            "  %s -i out/toto.txt nom\n",
+            argv[0], argv[0], argv[0]);
     return 1;
   }
 
-  int fd = open(argv[1], O_RDONLY);
+  int substring = 0;
+  char *filename;
+  char *search;
+
+  if (strcmp(argv[1], "-i") == 0) {
+    substring = 1;
+    if (argc < 4) {
+      fprintf(stderr, "Usage avec -i : %s -i <fichier> <mot>\n", argv[0]);
+      return 1;
+    }
+    filename = argv[2];
+    search = argv[3];
+  } else {
+    filename = argv[1];
+    search = argv[2];
+  }
+
+  int fd = open(filename, O_RDONLY);
   if (fd == -1) {
     perror("open");
     return 1;
@@ -33,25 +54,39 @@ int main(int argc, char *argv[]) {
   int wpos = 0;
   ssize_t byte_read;
   int word_count = 0;
+  int line_number = 1;
 
   while ((byte_read = read(fd, buffer, BUF_SIZE)) > 0) {
     for (int i = 0; i < byte_read; i++) {
-      char c = buffer[i];
+      char caracter = buffer[i];
 
-      if (isEndOfSentence(c)) {
+      if (caracter == '\n')
+        line_number++;
+
+      if (isEndOfSentence(caracter)) {
         if (wpos > 0) {
           word[wpos] = '\0';
 
-          if (strcmp(argv[2], word) == 0) {
+          int found = 0;
+          if (substring) {
+            if (strstr(word, search) != NULL)
+              found = 1;
+          } else {
+            if (strcmp(word, search) == 0)
+              found = 1;
+          }
+
+          if (found) {
             word_count++;
             strcpy(word_found, word);
+            printf("Mot trouvé : '%s' à la ligne %d\n", word, line_number);
           }
 
           wpos = 0;
         }
       } else {
         if (wpos < WORD_SIZE - 1) {
-          word[wpos++] = c;
+          word[wpos++] = caracter;
         }
       }
     }
@@ -59,16 +94,27 @@ int main(int argc, char *argv[]) {
 
   if (wpos > 0) {
     word[wpos] = '\0';
-    if (strcmp(argv[2], word) == 0) {
+
+    int found = 0;
+    if (substring) {
+      if (strstr(word, search) != NULL)
+        found = 1;
+    } else {
+      if (strcmp(word, search) == 0)
+        found = 1;
+    }
+
+    if (found) {
       word_count++;
       strcpy(word_found, word);
+      printf("Mot trouvé : '%s' à la ligne %d\n", word, line_number);
     }
   }
 
   if (word_count > 0)
-    printf("Mot trouvé : %s, nombres : %d\n", word_found, word_count);
+    printf("\nNombre total d'occurrences : %d\n", word_count);
   else
-    printf("Mot '%s' non trouvé\n", argv[2]);
+    printf("Mot '%s' non trouvé\n", search);
 
   close(fd);
   return 0;
